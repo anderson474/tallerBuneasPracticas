7274de8347dec5a1c3f06251962d8ad0f1ee0e28
= <=>
2022-10-10 08:39:01 -0500

    creacon de canciones

---
+++ src/Cancion.java
@@ -0,0 +1,41 @@
+import java.time.Duration;
+import java.time.LocalDate;
+
+
+public class Cancion {
+    private String title;
+    private int id;
+    private LocalDate fecha;
+    private int duracion;
+    private String genero;
+    private String caratula;
+    private String descripcion;
+    private static int contador=0;
+
+
+    public Cancion(String title,CharSequence fecha, int duracion, String genero,
+                   String caratula, String descripcion) {
+
+        this.title = title;
+        this.fecha = LocalDate.parse(fecha);
+        this.duracion = duracion;
+        this.genero = genero;
+        this.caratula = caratula;
+        this.descripcion = descripcion;
+        contador=contador+1;
+        this.id=contador;
+    }
+
+    @Override
+    public String toString() {
+        return "Cancion{" +
+                "title='" + title + '\'' +
+                ", id=" + id +
+                ", fecha=" + fecha +
+                ", duracion=" + duracion +
+                ", genero='" + genero + '\'' +
+                ", caratula='" + caratula + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
---
+++ src/ManejoBiblioteca.java
@@ -0,0 +1,14 @@
+public class ManejoBiblioteca {
+    public static void main(String[] args) {
+        Cancion c1 = new Cancion("Laugh Now Cry Later","2022-01-01",1000,"urbano","caratula"
+        ,"esta es una cancion");
+        Cancion c2 = new Cancion("Laugh Now Cryed Later","2021-01-01",100,"urbano","caratula1"
+                ,"esta es una cancion2");
+        System.out.println(c1);
+        System.out.println(c2);
+
+
+
+
+    }
+}
