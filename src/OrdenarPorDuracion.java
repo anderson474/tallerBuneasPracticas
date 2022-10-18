179eddd71df53e510e93fb9906a19df4361b3af5
= <=>
2022-10-18 13:19:26 -0500

    documentando las clases OrdenarPorduracion y OrdenarPorFecha

--- src/OrdenarPorDuracion.java
+++ src/OrdenarPorDuracion.java
@@ -1,5 +1,23 @@
 import java.util.Comparator;
-
+/**
+ * [Esta clase permite comparar dos canciones,
+ * para ordenarlas de acuerdo a su atributo duracion,
+ * las canciones se ordenan de acuerdo al valor
+ * retornado por la clase.
+ * En la biblioteca se implementa en el metodo
+ * Collection.sort( array, comparador), el cual recibe
+ * el array a ordenar y la clase de comparacion.
+ * 
+ * ]
+ *
+ * @version [1.0.0]
+ *
+ * @author [Jhon Anderson Galeano Santillana -jhon.galeano201@tau.usbmed.edu.co
+ *          Juan Guillermo Morales  -]
+ *
+ * @since [1.0.0]
+ *
+ */
 public class OrdenarPorDuracion implements Comparator<Cancion> {
     public int compare(Cancion a, Cancion b) {
         return a.getDuracion() - b.getDuracion();
